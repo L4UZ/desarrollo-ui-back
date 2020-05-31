f4cbd6410ee1e2b79810a71a25adf885fbb773c2
Diego Lauz <diego.lauz@hotmail.com>
2020-05-31 18:26:35 -0300

    Sandbox script added

---
+++ src/sandbox.js
@@ -0,0 +1,35 @@
+import open from 'open';
+
+import readline from 'readline-sync';
+
+const fs = require('fs');
+
+const rawdata = fs.readFileSync('C:\\Users\\diego\\Desktop\\db-with-locations.json');
+const data = JSON.parse(rawdata);
+
+let res = '';
+const fixes = [];
+
+data.map(place => {
+  if (res !== 'q') {
+    const {
+      location: {
+        coordinates: [longitude, latitude],
+      },
+    } = place;
+    console.log(place.name, latitude, longitude);
+
+    open(
+      encodeURI(
+        `https://google.com/maps/search/${latitude},+${longitude}/@${latitude},${longitude},10z`,
+      ),
+    );
+
+    res = readline.question('Next? (q=quit)');
+
+    if (res && res !== 'q') {
+      fs.appendFileSync('./fixes.txt', `\nid: ${place._id.$oid}, name: ${place.name}, ${res}`);
+      fixes.push();
+    }
+  }
+});
