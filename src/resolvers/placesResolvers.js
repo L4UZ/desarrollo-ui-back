d3df4bf51abfcbce6ebefcad435ae4e954869bb1
Diego Lauz <diego.lauz@hotmail.com>
2020-06-01 16:17:48 -0300

    Removed `spherical; true`

--- src/resolvers/placesResolvers.js
+++ src/resolvers/placesResolvers.js
@@ -14,7 +14,6 @@ export const placesResolvers = {
               coordinates: [longitude, latitude],
             },
             distanceField: 'distance',
-            spherical: true,
           },
         },
       ]),
